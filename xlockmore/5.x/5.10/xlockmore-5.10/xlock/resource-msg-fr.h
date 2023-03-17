fa61469c7ee85f35e5304c73a4e302e9e7b8fe41
dpr <driesz@esri.com>
2023-03-17 16:20:51 -0700

    added 5.10

---
+++ xlockmore/5.x/5.10/xlockmore-5.10/xlock/resource-msg-fr.h
@@ -0,0 +1,27 @@
+/*
+ * Resource messages for French
+ */
+
+#ifndef __RESOURCE_MSG_FR__
+#define __RESOURCE_MSG_FR__
+
+#define DEF_NAME		"Nom: "
+#define DEF_PASS		"Mot de passe: "
+#define DEF_VALID		"Validation ..."
+#define DEF_INVALID		"Mot de passe Invalide."
+#define DEF_INVALIDCAPSLOCK	"Mot de passe Invalide, Caps Lock on."
+#define DEF_INFO		"Entrez le mot de passe ou choisissez l'icone pour verrouiller."
+
+#ifdef HAVE_KRB5
+/* FIXME: translate to french */
+#define DEF_KRBINFO		"Enter Kerberos password to unlock; select icon to lock."
+#endif /* HAVE_KRB5 */
+
+#define DEF_COUNT_FAILED	" essais échoué."
+#define DEF_COUNTS_FAILED	" essais échoués."
+
+#define DEF_BTN_LABEL		"Logout"
+#define DEF_BTN_HELP		"Cliquer ici pour etre deloger"
+#define DEF_FAIL		"Auto-logout a echoue"
+
+#endif /* __RESOURCE_MSG_FR__ */
