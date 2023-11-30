0c33b54974f1e648b10ddd679bfa39893d2f0b6f
dpr <dave@riesz.net>
2023-11-30 17:58:05 +0000

    added 5.74

---
+++ xlockmore/5.x/5.74/xlockmore-5.74/modes/glx/i_evert.h
@@ -0,0 +1,7 @@
+#include <GL/gl.h>
+
+extern double scale;
+extern double width;
+extern int sphere;
+extern int torscene;
+extern double corrstart, pushstart, twiststart, untwiststart, uncorrstart;
